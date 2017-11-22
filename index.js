afa6278cf987cb96e96acf9a3664b83549a1e9df
Richard Loera <richexp9@gmx.com>
2017-11-22 01:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object.key = value
   return object
 }
-// 
-// function deleteFromObjectByKey(object, key) {
-//   var newObj = Object.assign(object, )
-// }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign(object, )
+}
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
