8e7e77f3a6ea1c1d50020e2a2f502d9529cb39f4
Richard Loera <richexp9@gmx.com>
2017-11-22 01:26:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  return object
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
